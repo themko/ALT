2bcbb32a9b3da2cc23714bbe37febd8896124a0a
Peter Dekker <pd.double12@gmail.com>
2016-09-23 12:39:11 +0200

    transition probabilities inputs and outputs alignments

--- ass1/transition_probabilities.py
+++ ass1/transition_probabilities.py
@@ -2,12 +2,12 @@ from collections import defaultdict
 
 def transition_probabilities():
     print 'start'
-    with open('phrases.txt', 'r') as phrases_file: #open the file
+    with open('phrases_with_alignments.txt', 'r') as phrases_file: #open the file
         phrases = phrases_file.readlines()    
     
-    with open("transition_probs.txt","w") as probs_file:
+    with open("transition_probs_with_alignments.txt","w") as probs_file:
         for line in phrases:
-            f,e, probs = line.split("|||")
+            f,e, probs, alignments = line.split("|||")
             prob_f, prob_e, prob_f_e = probs.split()
             # P(F|E) = P(F,E) / P(E)
             prob_f_given_e = float(prob_f_e) / float(prob_e)
@@ -16,6 +16,6 @@ def transition_probabilities():
             prob_e_given_f = float(prob_f_e) / float(prob_f)
             
             # Write line to file
-            probs_file.write(f + " ||| " + e + " ||| " +  str(prob_f_given_e) + " " + str(prob_e_given_f) + "\n")
+            probs_file.write(f + " ||| " + e + " ||| " +  str(prob_f_given_e) + " " + str(prob_e_given_f) + " ||| " + alignments + "\n")
 
 transition_probabilities()
