48fc12478f9e08d04492a21b4da9df95fcf1f72f
Peter Dekker <pd.double12@gmail.com>
2016-09-26 11:55:12 +0200

    lower memory consumption and different output format for combine.py

--- ass1/combine.py
+++ ass1/combine.py
@@ -4,33 +4,36 @@ def combine():
     print 'start'
     
     #Read in phrases
-    with open('phrases_with_alignments.txt', 'r') as phrase_file: 
-        input_lines = phrase_file.readlines() 
-    print 'read input'
-    
-    #Generate freq dictionaries
     dictF ={}
     dictE ={}
     dictFE={}
-    for line in input_lines:
-        f,e, freqs, alignments = line.split(" ||| ")
-        #print f,len(f)
-        freq_f, freq_e, freq_f_e = freqs.split()
-        #print prob_f,prob_e,prob_f_e
-        dictF[f]=freq_f
-        dictE[e]=freq_e
-        dictFE[(f,e)]=freq_f_e
+    with open('phrases_with_alignments.txt', 'r') as phrase_file:
+        print "Reading phrases file..."
+        for line in phrase_file:
+            #Generate freq dictionaries
+            f,e, freqs, alignments = line.split(" ||| ")
+            f = f.strip()
+            e = e.strip()
+            #print f,len(f)
+            freq_f, freq_e, freq_f_e = freqs.split()
+            #print prob_f,prob_e,prob_f_e
+            dictF[f]=freq_f
+            dictE[e]=freq_e
+            dictFE[(f,e)]=freq_f_e
         
+    print "Now writing to file..."
     #REPLACE WITH ACTUAL OUTPUT FILE
-    with open("kmo_output.txt") as kmo_file:
-        kmo_lines = kmo_file.readlines()
-    #Write frequencies behind kmo_output-line in new file
-    with open("combine_output.txt","w") as combine_file:
-        for kmo_line in kmo_lines[0:10]:
-            #Possibly remove aligns, if they are not included in the actual KMO file
-            f,e, probs,aligns = kmo_line.split(" ||| ")
-            freq_f = dictF[f]
-            freq_e = dictE[e]
-            freq_f_e = dictFE[(f,e)]
-            combine_file.write(kmo_line.rstrip() + " ||| " + freq_f + freq_e + freq_f_e+"\n")
+    with open("lexical_weights_with_alignments.txt") as kmo_file:
+        #Write frequencies behind kmo_output-line in new file
+        with open("combine_output.txt","w") as combine_file:
+            for kmo_line in kmo_file:
+                #Possibly remove aligns, if they are not included in the actual KMO file
+                f,e, probs,aligns = kmo_line.split(" ||| ")
+                f = f.strip()
+                e = e.strip()
+                probs = probs.strip()
+                freq_f = dictF[f]
+                freq_e = dictE[e]
+                freq_f_e = dictFE[(f,e)]
+                combine_file.write(f + " ||| " + e + " ||| " + probs + " ||| " + freq_f + " " + freq_e + " "+ freq_f_e+"\n")
 combine()
